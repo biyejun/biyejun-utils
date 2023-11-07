6ce5cd3cd5b9548ba77aa6db5eb79d4504f07017
biyejun <bnbiye@163.com>
2023-11-07 18:28:16 +0800

    test: test

--- packages/biye-test/index.js
+++ packages/biye-test/index.js
@@ -2,3 +2,4 @@ const { testVal } = require('@biye/biye-demo/export-test')
 const { testVal: testVal2 } = require('@biye/biye-demo/test')
 console.log(testVal, 'testVal');
 console.log(testVal2, 'testVal2');
+console.log('test11');
